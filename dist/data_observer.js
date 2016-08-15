c7107c054bc146f2dbd93144324ce4d1f0621bd9
Stephen Darnell <stephen.darnell@outlook.com>
2016-08-15 14:09:49 +0100

    Hack to add PromiseCompleter and PromiseWrapper

--- dist/data_observer.js
+++ dist/data_observer.js
@@ -1,6 +1,49 @@
 'use strict';
-var async_1 = require('@angular/core/src/facade/async');
 var utils_1 = require('./utils');
+var PromiseWrapper = (function () {
+    function PromiseWrapper() {
+    }
+    PromiseWrapper.resolve = function (obj) { return Promise.resolve(obj); };
+    PromiseWrapper.reject = function (obj, _) { return Promise.reject(obj); };
+    // Note: We can't rename this method into `catch`, as this is not a valid
+    // method name in Dart.
+    PromiseWrapper.catchError = function (promise, onError) {
+        return promise.catch(onError);
+    };
+    PromiseWrapper.all = function (promises) {
+        if (promises.length == 0)
+            return Promise.resolve([]);
+        return Promise.all(promises);
+    };
+    PromiseWrapper.then = function (promise, success, rejection) {
+        return promise.then(success, rejection);
+    };
+    PromiseWrapper.wrap = function (computation) {
+        return new Promise(function (res, rej) {
+            try {
+                res(computation());
+            }
+            catch (e) {
+                rej(e);
+            }
+        });
+    };
+    PromiseWrapper.scheduleMicrotask = function (computation) {
+        PromiseWrapper.then(PromiseWrapper.resolve(null), computation, function (_) { });
+    };
+    PromiseWrapper.isPromise = function (obj) { return obj instanceof Promise; };
+    PromiseWrapper.completer = function () {
+        var resolve;
+        var reject;
+        var p = new Promise(function (res, rej) {
+            resolve = res;
+            reject = rej;
+        });
+        return { promise: p, resolve: resolve, reject: reject };
+    };
+    return PromiseWrapper;
+}());
+exports.PromiseWrapper = PromiseWrapper;
 /**
  * A helper class for data loading events.
  * For example, used in @MeteorComponent to wrap callbacks
@@ -13,7 +56,7 @@ var DataObserver = (function () {
     DataObserver.pushCb = function (callbacks) {
         var _this = this;
         utils_1.check(callbacks, utils_1.Match.Where(utils_1.isMeteorCallbacks));
-        var completer = async_1.PromiseWrapper.completer();
+        var completer = PromiseWrapper.completer();
         var dequeue = function (promise) {
             var index = _this._promises.indexOf(promise);
             if (index !== -1) {
